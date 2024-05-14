e35f20b05a2c8fb3bf9069f9b42830bf5d54669e
Jaxswat <contact@jaxswat.com>
2024-05-14 16:56:01 -0400

    Add eye blinking

---
+++ addon/content/models/elizabeth_beach/liz_anim.lua
@@ -0,0 +1,19 @@
+DoIncludeScript( "animation/sequence_functions/sequence_functions", getfenv(1) )
+
+---------------------------- liz stuff
+
+model:CreatePoseParameter( "head_pitch", -30, 30, 0, false )
+model:CreatePoseParameter( "head_yaw", -60, 60, 0, false )
+PoseMatrixFromSequence( { model = model, name = "head_look_seq", pose_x = "head_pitch", pose_y = "head_yaw", subtract = "head_look", subtractframe = 4, source = "head_look", hidden = false, autoplay = true, righttoleft = true } )
+
+model:CreatePoseParameter( "eyes_pitch", -42, 48, 0, false )
+model:CreatePoseParameter( "eyes_yaw", -50, 50, 0, false )
+PoseMatrixFromSequence( { model = model, name = "eyes_look_seq", pose_x = "eyes_pitch", pose_y = "eyes_yaw", subtract = "eyes_look", subtractframe = 4, source = "eyes_look", hidden = false, autoplay = true, righttoleft = true } )
+
+model:CreatePoseParameter( "face_smile", 0, 1, 0, false )
+PoseMatrixFromSequence( { model = model, name = "face_smile_seq", pose_x = "face_smile", source = "smile", subtract = "smile", subtractframe = 0, hidden = false, numrows = 1, numcolumns = 2, autoplay = true, righttoleft = true } )
+
+model:CreatePoseParameter( "face_blink", 0, 1, 0, false )
+PoseMatrixFromSequence( { model = model, name = "face_blink_seq", pose_x = "face_blink", source = "blink", subtract = "blink", subtractframe = 0, hidden = false, numrows = 1, numcolumns = 2, autoplay = true, righttoleft = true } )
+
+print("BUILT ELIZABETH ANIMS")
--- vscript/src/entities/elizabeth/components/EmotionComponent.ts
+++ vscript/src/entities/elizabeth/components/EmotionComponent.ts
@@ -1,18 +1,99 @@
 import LizComponent from "./LizComponent";
 import Elizabeth from "../Elizabeth";
 
+class BlinkingAnimation {
+
+    // Minimum duration of a single blink in seconds
+    private blinkDurationMin: number;
+    // Maximum duration of a single blink in seconds
+    private blinkDurationMax: number;
+    // Minimum interval between blinks in seconds
+    private blinkIntervalMin: number;
+    // Maximum interval between blinks in seconds
+    private blinkIntervalMax: number;
+
+    private blinkTimer: number;
+    private blinkPercentage: number;
+    private isBlinking: boolean;
+    private currentBlinkDuration: number;
+    private currentBlinkInterval: number;
+
+    public constructor() {
+        this.blinkDurationMin = 0.1;
+        this.blinkDurationMax = 0.4;
+        this.blinkIntervalMin = 2;
+        this.blinkIntervalMax = 8;
+        this.blinkTimer = 0;
+        this.blinkPercentage = 0;
+        this.isBlinking = false;
+        this.currentBlinkDuration = 0;
+        this.currentBlinkInterval = this.getRandomBlinkInterval();
+    }
+
+    public update(delta: number): number {
+        this.blinkTimer += delta;
+
+        if (!this.isBlinking && this.blinkTimer >= this.currentBlinkInterval) {
+            this.startBlink();
+        }
+
+        if (this.isBlinking) {
+            this.updateBlinkPercentage(delta);
+        }
+
+        return this.blinkPercentage;
+    }
+
+    private startBlink(): void {
+        this.isBlinking = true;
+        this.blinkTimer = 0;
+        this.blinkPercentage = 0;
+        this.currentBlinkDuration = this.getRandomBlinkDuration();
+        this.currentBlinkInterval = this.getRandomBlinkInterval();
+    }
+
+    private updateBlinkPercentage(delta: number): void {
+        const halfDuration = this.currentBlinkDuration / 2;
+
+        if (this.blinkTimer < halfDuration) {
+            // Eyelids closing
+            this.blinkPercentage = this.blinkTimer / halfDuration;
+        } else if (this.blinkTimer < this.currentBlinkDuration) {
+            // Eyelids opening
+            this.blinkPercentage = 1 - (this.blinkTimer - halfDuration) / halfDuration;
+        } else {
+            // Blink done
+            this.isBlinking = false;
+            this.blinkPercentage = 0;
+        }
+    }
+
+    private getRandomBlinkDuration(): number {
+        return Math.random() * (this.blinkDurationMax - this.blinkDurationMin) + this.blinkDurationMin;
+    }
+
+    private getRandomBlinkInterval(): number {
+        return Math.random() * (this.blinkIntervalMax - this.blinkIntervalMin) + this.blinkIntervalMin;
+    }
+}
+
 export default class EmotionComponent extends LizComponent {
     private addSmile: boolean;
     private smile: number;
+
+    private blinkingAnimation: BlinkingAnimation;
 
     public constructor(liz: Elizabeth) {
         super(liz);
 
         this.addSmile = true;
         this.smile = 0;
+        this.blinkingAnimation = new BlinkingAnimation();
     }
 
     public updatePose(delta: number): void {
+        const blink = this.blinkingAnimation.update(delta);
+
         if (this.smile >= 1) {
             this.addSmile = false;
         } else if (this.smile <= 0){
@@ -26,6 +107,7 @@ export default class EmotionComponent extends LizComponent {
         }
 
         this.liz.getEntity().SetPoseParameter("face_smile", this.smile);
+        this.liz.getEntity().SetPoseParameter("face_blink", blink);
     }
 
     /**
